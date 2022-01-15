b0696b608adf4c50cba06ba97aa10bafb9dee069
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2022-01-15 20:30:33 -0300

    end heroku

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const session = require("express-session");
 const passport = require("passport");
 const flash = require('connect-flash');
 const dotenv = require('dotenv')
-const urlDb = require('../members-only-top/dbPassword')
 var helmet = require('helmet');
 var compression = require('compression');
 dotenv.config()
 
-var mongoDB = urlDb
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
